07ff31334caa809f6da8f34f0afa479ce0d5a7de
Phillip Garcia <phillipthomasgarcia@gmail.com>
2016-08-18 16:06:31 -0500

    routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/sayhello/{name?}', function($name =  'World')
+{
+    if ($name == "Thomas") {
+        return redirect('/');
+    }
+    return "Hello, $name!";
+});
+Route::get('/uppercase/{word?}', function($word = 'WORD')
+{
+    $caps = strtoupper($word);
+    return "This: $caps, is uppercased";
+});
+Route::get('/increment/{number?}', function($number = 'number')
+{
+    $number = $number + 1;
+    return "This: $number, has been incremented from your input.";
+});
+Route::get('/add/{number1}/{number2}', function($number1, $number2)
+{
+    $sum = $number1 + $number2;
+    return "The sum of $number1 and $number2 is $sum";
+});
