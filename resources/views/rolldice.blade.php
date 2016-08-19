4a8753c3e80426e33347ae3bf8d0eca040295537
Phillip Garcia <phillipthomasgarcia@gmail.com>
2016-08-19 10:36:31 -0500

    added link options to roll dice game

--- resources/views/rolldice.blade.php
+++ resources/views/rolldice.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.master')
 @section('content')
-<h1>Dice roll : <?php echo $number; ?>!</h1>
-<h3>You guessed : <?php echo $guess; ?>!</h3>
-<div>
-    <?php if ($guess == $number) { ?>
-        <?php echo "You win" ?>
-    <?php } else { ?>
-        <?php echo "Try again." ?>
-    <?php } ?>
-</div >
+    <h1>Dice roll was, <?php echo $number; ?>!</h1>
+    <h1>You guessed, <?php echo $guess; ?>!</h1>
+    <div>
+        <?php if ($guess == $number) { ?>
+            <?php echo "YOU GOT IT!" ?>
+        <?php } else { ?>
+            <?php echo "Try again." ?>
+        <?php } ?>
+    </div >
+    <ul>
+        <li><a href="{{ action('HomeController@rollDice', 1) }}">1</a></li>
+        <li><a href="{{ action('HomeController@rollDice', 2) }}">2</a></li>
+        <li><a href="{{ action('HomeController@rollDice', 3) }}">3</a></li>
+        <li><a href="{{ action('HomeController@rollDice', 4) }}">4</a></li>
+        <li><a href="{{ action('HomeController@rollDice', 5) }}">5</a></li>
+        <li><a href="{{ action('HomeController@rollDice', 6) }}">6</a></li>
+    </ul>
 @stop
